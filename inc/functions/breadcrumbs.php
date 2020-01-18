8a0d344dcedfa53cc4c3c1939784904fc82551bb
N1Tech <n1tech@N1Techs-Mac-mini.local>
2020-01-18 18:37:26 +0545

    updated text domain for breadcrumb

--- inc/functions/breadcrumbs.php
+++ inc/functions/breadcrumbs.php
@@ -282,18 +282,18 @@ class Everestthemes_Breadcrumb_Trail {
 	protected function set_labels() {
 
 		$defaults = array(
-			'aria_label'          	=> esc_attr_x( 'Breadcrumbs', 'breadcrumbs aria label', 'viable-blog-pro' ),
-			'home'           		=> esc_html__( 'Home', 'viable-blog-pro' ),
-			'error_404'           	=> esc_html__( '404 Not Found', 'viable-blog-pro' ),
-			'archives'            	=> esc_html__( 'Archives', 'viable-blog-pro' ),
+			'aria_label'          	=> esc_attr_x( 'Breadcrumbs', 'breadcrumbs aria label', 'viable-blog' ),
+			'home'           		=> esc_html__( 'Home', 'viable-blog' ),
+			'error_404'           	=> esc_html__( '404 Not Found', 'viable-blog' ),
+			'archives'            	=> esc_html__( 'Archives', 'viable-blog' ),
 			// Translators: %s is the search query. The HTML entities are opening and closing curly quotes.
-			'search'           		=> esc_html__( 'Search results for: ', 'viable-blog-pro' ),
+			'search'           		=> esc_html__( 'Search results for: ', 'viable-blog' ),
 			// Translators: %s is the page number.
-			'paged'               	=> esc_html__( 'Page %s',                               'viable-blog-pro' ),
+			'paged'               	=> esc_html__( 'Page %s',                               'viable-blog' ),
 			// Translators: Minute archive title. %s is the minute time format.
-			'archive_minute'      	=> esc_html__( 'Minute %s',                             'viable-blog-pro' ),
+			'archive_minute'      	=> esc_html__( 'Minute %s',                             'viable-blog' ),
 			// Translators: Weekly archive title. %s is the week date format.
-			'archive_week'        	=> esc_html__( 'Week %s',                               'viable-blog-pro' ),
+			'archive_week'        	=> esc_html__( 'Week %s',                               'viable-blog' ),
 
 			// "%s" is replaced with the translated date/time format.
 			'archive_minute_hour' 	=> '%s',
@@ -731,7 +731,7 @@ class Everestthemes_Breadcrumb_Trail {
 
 		// Add the minute + hour item.
 		if ( true === $this->args['show_title'] )
-			$this->items[] = sprintf( $this->labels['archive_minute_hour'], get_the_time( esc_html_x( 'g:i a', 'minute and hour archives time format', 'viable-blog-pro' ) ) );
+			$this->items[] = sprintf( $this->labels['archive_minute_hour'], get_the_time( esc_html_x( 'g:i a', 'minute and hour archives time format', 'viable-blog' ) ) );
 	}
 
 	/**
@@ -748,7 +748,7 @@ class Everestthemes_Breadcrumb_Trail {
 
 		// Add the minute item.
 		if ( true === $this->args['show_title'] )
-			$this->items[] = sprintf( $this->labels['archive_minute'], get_the_time( esc_html_x( 'i', 'minute archives time format', 'viable-blog-pro' ) ) );
+			$this->items[] = sprintf( $this->labels['archive_minute'], get_the_time( esc_html_x( 'i', 'minute archives time format', 'viable-blog' ) ) );
 	}
 
 	/**
@@ -765,7 +765,7 @@ class Everestthemes_Breadcrumb_Trail {
 
 		// Add the hour item.
 		if ( true === $this->args['show_title'] )
-			$this->items[] = sprintf( $this->labels['archive_hour'], get_the_time( esc_html_x( 'g a', 'hour archives time format', 'viable-blog-pro' ) ) );
+			$this->items[] = sprintf( $this->labels['archive_hour'], get_the_time( esc_html_x( 'g a', 'hour archives time format', 'viable-blog' ) ) );
 	}
 
 	/**
@@ -781,9 +781,9 @@ class Everestthemes_Breadcrumb_Trail {
 		$this->add_rewrite_front_items();
 
 		// Get year, month, and day.
-		$year  = sprintf( $this->labels['archive_year'],  get_the_time( esc_html_x( 'Y', 'yearly archives date format',  'viable-blog-pro' ) ) );
-		$month = sprintf( $this->labels['archive_month'], get_the_time( esc_html_x( 'F', 'monthly archives date format', 'viable-blog-pro' ) ) );
-		$day   = sprintf( $this->labels['archive_day'],   get_the_time( esc_html_x( 'j', 'daily archives date format',   'viable-blog-pro' ) ) );
+		$year  = sprintf( $this->labels['archive_year'],  get_the_time( esc_html_x( 'Y', 'yearly archives date format',  'viable-blog' ) ) );
+		$month = sprintf( $this->labels['archive_month'], get_the_time( esc_html_x( 'F', 'monthly archives date format', 'viable-blog' ) ) );
+		$day   = sprintf( $this->labels['archive_day'],   get_the_time( esc_html_x( 'j', 'daily archives date format',   'viable-blog' ) ) );
 
 		// Add the year and month items.
 		$this->items[] = sprintf( '<a href="%s">%s</a>', esc_url( get_year_link( get_the_time( 'Y' ) ) ), $year );
@@ -809,8 +809,8 @@ class Everestthemes_Breadcrumb_Trail {
 		$this->add_rewrite_front_items();
 
 		// Get the year and week.
-		$year = sprintf( $this->labels['archive_year'],  get_the_time( esc_html_x( 'Y', 'yearly archives date format', 'viable-blog-pro' ) ) );
-		$week = sprintf( $this->labels['archive_week'],  get_the_time( esc_html_x( 'W', 'weekly archives date format', 'viable-blog-pro' ) ) );
+		$year = sprintf( $this->labels['archive_year'],  get_the_time( esc_html_x( 'Y', 'yearly archives date format', 'viable-blog' ) ) );
+		$week = sprintf( $this->labels['archive_week'],  get_the_time( esc_html_x( 'W', 'weekly archives date format', 'viable-blog' ) ) );
 
 		// Add the year item.
 		$this->items[] = sprintf( '<a href="%s">%s</a>', esc_url( get_year_link( get_the_time( 'Y' ) ) ), $year );
@@ -833,8 +833,8 @@ class Everestthemes_Breadcrumb_Trail {
 		$this->add_rewrite_front_items();
 
 		// Get the year and month.
-		$year  = sprintf( $this->labels['archive_year'],  get_the_time( esc_html_x( 'Y', 'yearly archives date format',  'viable-blog-pro' ) ) );
-		$month = sprintf( $this->labels['archive_month'], get_the_time( esc_html_x( 'F', 'monthly archives date format', 'viable-blog-pro' ) ) );
+		$year  = sprintf( $this->labels['archive_year'],  get_the_time( esc_html_x( 'Y', 'yearly archives date format',  'viable-blog' ) ) );
+		$month = sprintf( $this->labels['archive_month'], get_the_time( esc_html_x( 'F', 'monthly archives date format', 'viable-blog' ) ) );
 
 		// Add the year item.
 		$this->items[] = sprintf( '<a href="%s">%s</a>', esc_url( get_year_link( get_the_time( 'Y' ) ) ), $year );
@@ -857,7 +857,7 @@ class Everestthemes_Breadcrumb_Trail {
 		$this->add_rewrite_front_items();
 
 		// Get the year.
-		$year  = sprintf( $this->labels['archive_year'],  get_the_time( esc_html_x( 'Y', 'yearly archives date format',  'viable-blog-pro' ) ) );
+		$year  = sprintf( $this->labels['archive_year'],  get_the_time( esc_html_x( 'Y', 'yearly archives date format',  'viable-blog' ) ) );
 
 		// Add the year item.
 		if ( is_paged() || true === $this->args['show_title'] )
@@ -1191,15 +1191,15 @@ class Everestthemes_Breadcrumb_Trail {
 
 				// If using the %year% tag, add a link to the yearly archive.
 				if ( '%year%' == $tag )
-					$this->items[] = sprintf( '<a href="%s">%s</a>', esc_url( get_year_link( get_the_time( 'Y', $post_id ) ) ), sprintf( $this->labels['archive_year'], get_the_time( esc_html_x( 'Y', 'yearly archives date format',  'viable-blog-pro' ) ) ) );
+					$this->items[] = sprintf( '<a href="%s">%s</a>', esc_url( get_year_link( get_the_time( 'Y', $post_id ) ) ), sprintf( $this->labels['archive_year'], get_the_time( esc_html_x( 'Y', 'yearly archives date format',  'viable-blog' ) ) ) );
 
 				// If using the %monthnum% tag, add a link to the monthly archive.
 				elseif ( '%monthnum%' == $tag )
-					$this->items[] = sprintf( '<a href="%s">%s</a>', esc_url( get_month_link( get_the_time( 'Y', $post_id ), get_the_time( 'm', $post_id ) ) ), sprintf( $this->labels['archive_month'], get_the_time( esc_html_x( 'F', 'monthly archives date format', 'viable-blog-pro' ) ) ) );
+					$this->items[] = sprintf( '<a href="%s">%s</a>', esc_url( get_month_link( get_the_time( 'Y', $post_id ), get_the_time( 'm', $post_id ) ) ), sprintf( $this->labels['archive_month'], get_the_time( esc_html_x( 'F', 'monthly archives date format', 'viable-blog' ) ) ) );
 
 				// If using the %day% tag, add a link to the daily archive.
 				elseif ( '%day%' == $tag )
-					$this->items[] = sprintf( '<a href="%s">%s</a>', esc_url( get_day_link( get_the_time( 'Y', $post_id ), get_the_time( 'm', $post_id ), get_the_time( 'd', $post_id ) ) ), sprintf( $this->labels['archive_day'], get_the_time( esc_html_x( 'j', 'daily archives date format', 'viable-blog-pro' ) ) ) );
+					$this->items[] = sprintf( '<a href="%s">%s</a>', esc_url( get_day_link( get_the_time( 'Y', $post_id ), get_the_time( 'm', $post_id ), get_the_time( 'd', $post_id ) ) ), sprintf( $this->labels['archive_day'], get_the_time( esc_html_x( 'j', 'daily archives date format', 'viable-blog' ) ) ) );
 
 				// If using the %author% tag, add a link to the post author archive.
 				elseif ( '%author%' == $tag )
